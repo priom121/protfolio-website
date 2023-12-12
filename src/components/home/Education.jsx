26812331efb08a9a9c785d9b863e59a5c4b6566b
Ypriom121 <pritomgupta121@gmail.com>
2023-12-12 10:46:37 +0600

    modified

--- src/components/home/Education.jsx
+++ src/components/home/Education.jsx
@@ -4,7 +4,14 @@ const Education = () => {
 
  return (
  <div>
-    <h1 className="text-4xl text-cyan-500 font-bold">My Skills</h1>                                                                                      
+    <h1 className="text-4xl text-center text-cyan-500 font-bold">My Education</h1> 
+    <div  className="border-4 border-indigo-200 border-r-indigo-500 ">
+<a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+
+<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Website Development</h5>
+<p class="font-normal text-gray-700 dark:text-gray-400"> From front-end design to back-end development, I deliver high-quality, responsive websites that provide an exceptional user experience.</p>
+</a>
+</div>
  </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Contact from '../components/Contact';
 import Projects from '../components/Projects';
 import Footers from '../layouts/Footers';
 import Services from '../components/Services';
+import Education from '../components/home/Education';
 
 
 const Home = () => {
@@ -13,6 +14,7 @@ const Home = () => {
           <Projects/>
           <Services/>
           <Contact/>
+          <Education/>
         <Footers/>
         </div>
     );
